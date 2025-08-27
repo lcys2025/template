419a779904561dd55cf3d6c592cee2860c090951
lcyslcys <lcys2025@outlook.com>
2025-08-27 20:15:41 +0800

    Update codebase.sh

--- node_express_generator_blank/NodeJS-Project/codebase.sh
+++ node_express_generator_blank/NodeJS-Project/codebase.sh
@@ -1,1 +1,1 @@
-find . \( -path "./.git" -o -path ./documents \) -prune -o -type f \( -not -name "*.md" -a -not -name "*.css" -a -not -name "*.ico" -a -not -name ".gitignore" -a -not -name "codebase.*" \) -print -exec cat -b {} \; | grep '\S' >& codebase.txt
+find . \( -path "./.git" -o -path ./documents \) -prune -o -type f \( -not -name ".DS_Store" -a -not -name "*.md" -a -not -name "*.css" -a -not -name "*.ico" -a -not -name ".gitignore" -a -not -name "codebase.*" \) -print -exec cat -b {} \; | grep '\S' >& codebase.txt
